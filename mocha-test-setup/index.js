f3adafe3b09571235a923c5599ff69fb43e48e9f
A-Dolah <a.dolah.dev@gmail.com>
2020-09-07 14:12:13 +0200

    Revert "tesing"
    This reverts commit e8afb260d6326f3b27414fe28cf38a7aa42442f5.

--- mocha-test-setup/index.js
+++ mocha-test-setup/index.js
@@ -2,7 +2,7 @@ const testFunction = (names) => {
   const numberOfNamesStartingWithA = names.filter(
     (name) => name.slice(0, 1).toUpperCase() === 'A'
   ).length;
-  return numberOfNamesStartingWithA ? numberOfNamesStartingWithA : 'None';
+  // return numberOfNamesStartingWithA ? numberOfNamesStartingWithA : 'None';
   // Uncomment line above to pass tests
 };
 
